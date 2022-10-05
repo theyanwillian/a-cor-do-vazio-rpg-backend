8c999147c9eba6398ab8cb79d5ce5aab78c93f13
Yan <theyanwillian@gmail.com>
2022-10-04 23:52:39 -0300

    all

--- src/main/java/br/com/acordovaziobackend/controller/PlayerController.java
+++ src/main/java/br/com/acordovaziobackend/controller/PlayerController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin
 public class PlayerController {
     @Autowired
     private PlayerRepository playerRepository;
